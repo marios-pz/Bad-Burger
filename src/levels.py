374b6cb619462ba09f08571a13d682be29e75e52
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-07-29 13:16:38 +0300

    added multiple fruit maps

--- src/levels.py
+++ src/levels.py
@@ -1,68 +1,98 @@
 class Level1:
     begin_player_pos = [4, 5]
     path_map = "data/level-maps/level1/map.txt"
-    path_frtuits = "data/level-maps/level1/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt"
+    ]
     enemies = []
 
 
 class Level2:
     begin_player_pos = [2, 3]
     path_map = "data/level-maps/level2/map.txt"
-    path_frtuits = "data/level-maps/level2/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt"
+    ]
     enemies = []
 
 
 class Level3:
     begin_player_pos = [2, 2]
     path_map = "data/level-maps/level3/map.txt"
-    path_frtuits = "data/level-maps/level3/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt"
+    ]
     enemies = []
 
 
 class Level4:
     begin_player_pos = [10, 5]
     path_map = "data/level-maps/level4/map.txt"
-    path_frtuits = "data/level-maps/level4/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+    ]
     enemies = []
 
 
 class Level5:
     begin_player_pos = [7, 8]
     path_map = "data/level-maps/level5/map.txt"
-    path_frtuits = "data/level-maps/level5/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+    ]
     enemies = []
 
 
 class Level6:
     begin_player_pos = [4, 5]
     path_map = "data/level-maps/level6/map.txt"
-    path_frtuits = "data/level-maps/level6/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+        "data/level-maps/level1/fruits_3.txt"
+    ]
     enemies = []
 
 
 class Level7:
     begin_player_pos = [7, 7]
     path_map = "data/level-maps/level7/map.txt"
-    path_frtuits = "data/level-maps/level7/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+        "data/level-maps/level1/fruits_3.txt"
+    ]
     enemies = []
 
 
 class Level8:
     begin_player_pos = [4, 5]
     path_map = "data/level-maps/level8/map.txt"
-    path_frtuits = "data/level-maps/level8/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+    ]
     enemies = []
 
 
 class Level9:
     begin_player_pos = [12, 7]
     path_map = "data/level-maps/level9/map.txt"
-    path_frtuits = "data/level-maps/level9/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+    ]
     enemies = []
 
 
 class Level10:
     begin_player_pos = [12, 3]
     path_map = "data/level-maps/level10/map.txt"
-    path_frtuits = "data/level-maps/level10/fruits.txt"
+    path_frtuits = [
+        "data/level-maps/level1/fruits_1.txt",
+        "data/level-maps/level1/fruits_2.txt"
+        "data/level-maps/level1/fruits_3.txt"
+    ]
     enemies = []
