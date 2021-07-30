81b0f1f41f2fbd2514dd9f446f6656d788cd7d31
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-07-30 13:33:49 +0300

    the enemy will change direction when it hits a obstacle

--- src/enemies.py
+++ src/enemies.py
@@ -1,17 +1,15 @@
-from copy import copy
 import pygame as pg
 import random
-from statistics import mean
 
 
 class Enemy1:
 
     def __init__(self, screen, player, tile_map, fruits, dep_pos, type):
-        
+
         self.screen = screen
 
         self.surface = pg.Surface((32, 32))
-        self.rect = self.surface.get_rect(topleft=(dep_pos[0]*32, dep_pos[1]*32))
+        self.rect = self.surface.get_rect(topleft=(dep_pos[0] * 32, dep_pos[1] * 32))
         self.index = list(dep_pos)
 
         self.fruits = fruits
@@ -41,7 +39,7 @@ class Enemy1:
                 self.index[0] += 1
                 self.moving = True
             self.direction = "right"
-                
+
     def move_left(self):
         if not self.moving:
             if not self.coll_left():
@@ -65,51 +63,51 @@ class Enemy1:
 
     def coll_right(self):
         id_x, id_y = self.index
-        if id_x + 1 > len(self.player_grid[id_y])-2 or self.player_grid[id_y][id_x+1] == 1:
+        if id_x + 1 > len(self.player_grid[id_y]) - 2 or self.player_grid[id_y][id_x + 1] == 1:
             return True
         return False
 
     def coll_left(self):
-        id_x, id_y = self.index  
-        if id_x - 1 < 1 or self.player_grid[id_y][id_x-1] == 1:
+        id_x, id_y = self.index
+        if id_x - 1 < 1 or self.player_grid[id_y][id_x - 1] == 1:
             return True
         return False
 
     def coll_up(self):
         id_x, id_y = self.index
-        if id_y - 1 < 1 or self.player_grid[id_y-1][id_x] == 1:
+        if id_y - 1 < 1 or self.player_grid[id_y - 1][id_x] == 1:
             return True
         return False
 
     def coll_down(self):
         id_x, id_y = self.index
-        if id_y + 1 > len(self.player_grid)-2 or self.player_grid[id_y+1][id_x] == 1:
+        if id_y + 1 > len(self.player_grid) - 2 or self.player_grid[id_y + 1][id_x] == 1:
             return True
         return False
 
     def manage_animation(self):
         if self.direction == "right":
-            if self.rect.x + (self.velocity) >= self.index[0]*self.TLS_X:
+            if self.rect.x + self.velocity >= self.index[0] * self.TLS_X:
                 self.moving = False
-                self.rect.x += (self.index[0]*self.TLS_X - self.rect.x)
+                self.rect.x += (self.index[0] * self.TLS_X - self.rect.x)
             else:
                 self.rect.x += self.velocity
         elif self.direction == "left":
-            if self.rect.x - self.velocity <= self.index[0]*self.TLS_X:
+            if self.rect.x - self.velocity <= self.index[0] * self.TLS_X:
                 self.moving = False
-                self.rect.x -= (self.rect.x - self.index[0]*self.TLS_X)
+                self.rect.x -= (self.rect.x - self.index[0] * self.TLS_X)
             else:
                 self.rect.x -= self.velocity
         elif self.direction == "up":
-            if self.rect.y - self.velocity <= self.index[1]*self.TLS_Y:
+            if self.rect.y - self.velocity <= self.index[1] * self.TLS_Y:
                 self.moving = False
-                self.rect.y -= (self.rect.y-self.index[1]*self.TLS_Y)
+                self.rect.y -= (self.rect.y - self.index[1] * self.TLS_Y)
             else:
                 self.rect.y -= self.velocity
         elif self.direction == "down":
-            if self.rect.y + self.velocity >= self.index[1]*self.TLS_Y:
+            if self.rect.y + self.velocity >= self.index[1] * self.TLS_Y:
                 self.moving = False
-                self.rect.y += (self.index[1]*self.TLS_Y-self.rect.y)
+                self.rect.y += (self.index[1] * self.TLS_Y - self.rect.y)
             else:
                 self.rect.y += self.velocity
 
@@ -120,72 +118,33 @@ class Enemy1:
                     if self.index == [id_c, id_r]:
                         self.eat_fruit([id_c, id_r])
 
-    def choose_move(self):
-        if self.target is None:
-            self.nearest = self.find_nearest()
-            self.nr_id = self.nearest[1]
-            self.target = self.nearest[0]
-
-        else:
-            if self.fruits.grid[self.nr_id[1]][self.nr_id[0]] is None:
-                self.target = None
-        
-        dx = self.index[0]-self.nr_id[0]
-        dy = self.index[1]-self.nr_id[1]
-
-        self.tr_dir_x = "left" if dx > 0 else "right"
-        self.tr_dir_y = "up" if dy > 0 else "down"
-
-        if dx == 0:
-            self.tr_dir_x = None
-        
-        if dy == 0:
-            self.tr_dir_y = None
-
-
-        if self.tr_dir_x == "left":
-            self.move_left()
-        elif self.tr_dir_x == "right":
-            self.move_right()
-        
-        if self.tr_dir_y == "up":
-            self.move_up()
-        elif self.tr_dir_y == "down":
-            self.move_down()
-
-    def find_nearest(self):
-        cur_min = None
-        cur_min_id = [1000, 1000]
-        for id_r, row in enumerate(self.fruits.grid):
-            for id_c, col in enumerate(row):
-                if col is not None and self.player_grid[id_r][id_c] == 0:
-                    if mean([abs(id_c-self.index[0]), abs(id_r-self.index[1])]) < mean([abs(cur_min_id[0]-self.index[0]), abs(cur_min_id[1]-self.index[1])]):
-                        cur_min = col
-                        cur_min_id = [id_c, id_r]
-   
-        return cur_min, cur_min_id
-    
-
-    def update(self):
-        self.player_grid = self.player.player_grid
-        self.collide_fruits()
-        self.choose_move()
-
+    def move(self):
         if self.moving:
             self.manage_animation()
         else:
-            move_choice = random.randint(1, 4)
-            if move_choice == 1:
-                self.move_down()
-            elif move_choice == 2:
+            if   not self.coll_right() and self.direction == "right":
                 self.move_right()
-            elif move_choice == 3:
+            elif not self.coll_left() and self.direction == "left":
                 self.move_left()
-            else:
+            elif not self.coll_up() and self.direction == "up":
                 self.move_up()
+            elif not self.coll_down() and self.direction == "down":
+                self.move_down()
 
-        nearest = self.find_nearest()
-        pg.draw.rect(self.screen, (0, 255, 0), [nearest[1][0]*32, nearest[1][1]*32, 32, 32])
+            else:
+                move_choice = random.randint(1, 4)
+                if move_choice == 1:
+                    self.move_down()
+                elif move_choice == 2:
+                    self.move_right()
+                elif move_choice == 3:
+                    self.move_left()
+                else:
+                    self.move_up()
+
+    def update(self):
+        self.collide_fruits()
+        self.player_grid = self.player.player_grid
+        self.move()
 
         self.screen.blit(self.surface, self.rect)
-
