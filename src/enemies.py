139701f9422ed5f494854b3037cacb0d8284bd9f
Marios <30930688+Marios325346@users.noreply.github.com>
2021-08-01 02:47:40 +0300

    Upgraded enemy 1

--- src/enemies.py
+++ src/enemies.py
@@ -6,7 +6,6 @@ from os import listdir
 import src.pathfinder as pfd
 from src.tiles import ColliderTiles as CT
 
-
 class Enemy1:
 
     def __init__(self, screen, player, tile_map, dep_pos, type):
@@ -15,34 +14,21 @@ class Enemy1:
         self.tile_map = tile_map
 
         self.surface = pg.Surface((32, 32))
-        self.rect = self.surface.get_rect(topleft=(dep_pos[0] * 32, dep_pos[1] * 32))
+        self.rect = self.surface.get_rect(center=(dep_pos[0] * 32, dep_pos[1] * 32)) # Pls move the enemy a little to the left if possible
         self.index = list(dep_pos)
 
-        self.player_grid = player.player_grid
-        self.player = player
+        self.player_grid, self.player = player.player_grid, player
+        self.direction = "right" # Default direction
+        self.moving =  self.delaying = False
+        self.velocity = self.delay_anim = self.index_anim = self.delayed = 0
+        self.TLS_Y, self.TLS_X = tile_map.TLS_Y, tile_map.TLS_X
+        self.target =  self.tr_dir_x = self.tr_dir_y  = self.nearest = self.nr_id = self.nr_obj = None
 
-        self.direction = "right"
-        self.moving = False
-        self.velocity = 5
-
-        self.TLS_Y = tile_map.TLS_Y
-        self.TLS_X = tile_map.TLS_X
-
-        self.target = None
-        self.tr_dir_x = None
-        self.tr_dir_y = None
-        self.nearest = None
-        self.nr_id = None
-        self.nr_obj = None
-
-        self.anim_down = [load_alpha(f"data/assets/enemy1_down/{file}") for file in listdir("data/assets/enemy1_down")]
-        self.anim_up = [load_alpha(f"data/assets/enemy1_up/{file}") for file in listdir("data/assets/enemy1_up")]
+        self.anim_down = [resizex(load_alpha(f"data/assets/enemy1_down/{file}"),1.5) for file in listdir("data/assets/enemy1_down")]
+        self.anim_up = [resizex(load_alpha(f"data/assets/enemy1_up/{file}"),1.5) for file in listdir("data/assets/enemy1_up")]
+        self.anim_left = [resizex(load_alpha(f'data/assets/enemy1_side/{file}'), 1.5) for file in listdir('data/assets/enemy1_side')]
+        self.anim_right = [pg.transform.flip(image, True, False).convert_alpha() for image in self.anim_left]
         self.current_time = pg.time.get_ticks()
-        self.delay_anim = 0
-        self.index_anim = 0
-
-        self.delayed = 0
-        self.delaying = False
 
     def break_ice(self, first_cell):
         self.player_grid = self.player.player_grid
@@ -93,27 +79,19 @@ class Enemy1:
 
     def coll_right(self):
         id_x, id_y = self.index
-        if id_x + 1 > len(self.player_grid[id_y]) - 2 or self.player_grid[id_y][id_x + 1] == 1:
-            return True
-        return False
+        return id_x + 1 > len(self.player_grid[id_y]) - 2 or self.player_grid[id_y][id_x + 1] == 1
 
     def coll_left(self):
         id_x, id_y = self.index
-        if id_x - 1 < 1 or self.player_grid[id_y][id_x - 1] == 1:
-            return True
-        return False
+        return id_x < 2 or self.player_grid[id_y][id_x - 1] == 1
 
     def coll_up(self):
         id_x, id_y = self.index
-        if id_y - 1 < 1 or self.player_grid[id_y - 1][id_x] == 1:
-            return True
-        return False
+        return id_y < 2 or self.player_grid[id_y - 1][id_x] == 1
 
     def coll_down(self):
         id_x, id_y = self.index
-        if id_y + 1 > len(self.player_grid) - 2 or self.player_grid[id_y + 1][id_x] == 1:
-            return True
-        return False
+        return id_y + 1 > len(self.player_grid) - 2 or self.player_grid[id_y + 1][id_x] == 1
 
     def manage_animation(self):
         if self.direction == "right":
@@ -145,7 +123,7 @@ class Enemy1:
         if self.moving:
             self.manage_animation()
         elif not self.radius_player_detection():
-            self.velocity = 3
+            self.velocity = 1.5
 
             if not self.coll_right() and self.direction == "right":
                 self.move_right()
@@ -167,34 +145,25 @@ class Enemy1:
                 else:
                     self.move_up()
         else:
-            self.velocity = 3
+            self.velocity = 2.25 # The speed increases by 75 %
 
             path = pfd.init_pathfinder(self.index, self.player.index, self.player.player_grid)
             if path is False:
                 
-                grid_without_ice = [[0 if type(block) is CT.IceBlock or block is None else 1 for block in self.tile_map.collider_tiles[_1]] for _1 in range(len(self.player_grid))]
-                for i in grid_without_ice:
-                    print(i)    
-
-                print("")
-
-                for i in self.player_grid:
-                    print(i)
-                
+                grid_without_ice = [[0 if type(block) is CT.IceBlock or block is None else 1 for block in self.tile_map.collider_tiles[_1]] for _1 in range(len(self.player_grid))]         
                 second_path = pfd.init_pathfinder(self.index, self.player.index, grid_without_ice)
-                dx, dy = self.index[0]-second_path[1][0], self.index[1]-second_path[1][1]
 
-                if dx < 0:
+                if self.index[0]-second_path[1][0] < 0: # dx
                     if not self.coll_right():
                         self.move_right()
                     else:
                         self.break_ice([self.index[0]+1, self.index[1]])
-                elif dx > 0:
+                elif self.index[0]-second_path[1][0] > 0: # dx
                     if not self.coll_left():
                         self.move_left()
                     else:
                         self.break_ice([self.index[0]-1, self.index[1]])
-                elif dy < 0:
+                elif self.index[1]-second_path[1][1] < 0: # dy
                     if not self.coll_down():
                         self.move_down()
                     else:
@@ -206,56 +175,38 @@ class Enemy1:
                         self.break_ice([self.index[0], self.index[1]+1])
 
             elif type(path) is list:
-
-                dx, dy = self.index[0]-path[1][0], self.index[1]-path[1][1]
-                
-                if dx < 0:
+                if self.index[0]-path[1][0] < 0:
                     self.move_right()
-                elif dx > 0:
+                elif self.index[0]-path[1][0] > 0:
                     self.move_left()
-                elif dy < 0:
+                elif self.index[1]-path[1][1] < 0:
                     self.move_down()
                 else:
                     self.move_up()
 
-    def radius_player_detection(self):
-        dx, dy = abs(self.player.index[0]-self.index[0]), abs(self.player.index[1]-self.index[1])
-        if dx <= 5 and dy <= 5:
-            return True
-        return False
+    def radius_player_detection(self): # Returns boolean based on Condition
+        return abs(self.player.index[0]-self.index[0]) <= 5 and abs(self.player.index[1]-self.index[1]) <= 5
 
     def animate(self):
         self.current_time = pg.time.get_ticks()
 
-        if self.moving:
-
-            if self.current_time - self.delay_anim > 70:
-                self.delay_anim = self.current_time
-
-                if self.direction == "down":
-                    self.index_anim = (self.index_anim + 1) % len(self.anim_down)
-                    self.surface = self.anim_down[self.index_anim]
-                elif self.direction == "up":
-                    self.index_anim = (self.index_anim + 1) % len(self.anim_up)
-                    self.surface = self.anim_up[self.index_anim]
-                elif self.direction == "right":
-                    self.index_anim = (self.index_anim + 1) % len(self.anim_down)
-                    self.surface = self.anim_down[self.index_anim]
-                elif self.direction == "left":
-                    self.index_anim = (self.index_anim + 1) % len(self.anim_down)
-                    self.surface = self.anim_down[self.index_anim]
+        if self.moving and self.current_time - self.delay_anim > 70:
+            self.delay_anim = self.current_time
+            self.index_anim = (self.index_anim + 1) % len(self.anim_down)
+            if self.direction == "down":
+                self.surface = self.anim_down[self.index_anim]
+            elif self.direction == "left":
+                self.surface = self.anim_left[self.index_anim]
+            elif self.direction == "right":
+                self.surface = self.anim_right[self.index_anim]
+            elif self.direction == "up":
+                self.surface = self.anim_up[self.index_anim]
                     
-
-
     def update(self):
         self.current_time = pg.time.get_ticks()
-
         self.player_grid = self.player.player_grid
         if not self.delaying:
-            self.move()
-            self.animate()
-        else:
-            if self.current_time - self.delayed > 500:
-                self.delaying = False
-
+            self.move(); self.animate()
+        elif self.current_time - self.delayed > 600:
+            self.delaying = False
         self.screen.blit(self.surface, self.rect)
